d2862b808309891321decdc83cf332c133e5fdf2
the f <feldrim@gmail.com>
2018-02-10 23:38:46 +0300

    Refactoring

--- Fiver.Security.AspIdentity/Services/Email/EmailSender.cs
+++ Fiver.Security.AspIdentity/Services/Email/EmailSender.cs
@@ -8,46 +8,51 @@ namespace Fiver.Security.AspIdentity.Services.Email
     public class EmailSender : IEmailSender
     {
         private readonly ILogger _logger;
-        private readonly string _sender;
 
         public EmailSender(ILogger<EmailSender> logger)
         {
-            _sender = "z.f.blkn@gmail.com";
-
-
-
-
-
             _logger = logger;
         }
 
         public Task SendEmailAsync(string to, string subject, string body)
         {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("CRM", "z.f.blkn@gmail.com"));
-            message.To.Add(new MailboxAddress("User", to));
-            message.Subject = subject;
+            var message = CreateMessage(to, subject, body);
 
-            message.Body = new TextPart("plain")
-            {
-                Text = body
-            };
-
-            var task = Task.Run(() =>
-            {
-                using (var client = new SmtpClient())
-                {
-                    client.Connect("smtp.gmail.com", 587, true);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    client.Authenticate(_sender, "Zafer3092");
-                    client.Send(message);
-                    client.Disconnect(true);
-                }
-            });
+            var task = Task.Run(() => SendEmail(message));
 
             _logger.LogInformation($"{body}");
             //return Task.CompletedTask;
             return task;
+        }
+
+        private void SendEmail(MimeMessage message)
+        {
+            // Should get params from config
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("z.f.blkn@gmail.com", "Zafer3092");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+
+        private static MimeMessage CreateMessage(string to, string subject, string body)
+        {
+            // Should get params from config
+            var message = new MimeMessage
+            {
+                Subject = subject,
+                Body = new TextPart("plain")
+                {
+                    Text = body,
+                    ContentType = { Charset = "UTF-8" },
+                }
+            };
+            message.From.Add(new MailboxAddress("CRM", "z.f.blkn@gmail.com"));
+            message.To.Add(new MailboxAddress("User", to));
+            return message;
         }
     }
 }
