10aa491569bf370f159093ff1b452f8500fae554
the f <feldrim@gmail.com>
2018-02-11 18:29:43 +0300

    Added db seeder

--- Fiver.Security.AspIdentity/Seeder.cs
+++ Fiver.Security.AspIdentity/Seeder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Fiver.Security.AspIdentity.Services.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fiver.Security.AspIdentity
@@ -11,9 +12,30 @@ namespace Fiver.Security.AspIdentity
         {
             var context = serviceProvider.GetRequiredService<AppIdentityDbContext>();
             context.Database.EnsureCreated();
-            if (context.Roles.Any()) return;
-            context.Roles.Add(new AppIdentityRole { Name = "DefaultUser", Description = "Every user is a default user.", NormalizedName = "DefaultUser".Normalize() });
-            context.Roles.Add(new AppIdentityRole { Name = "Admin", Description = "At least one admin is needed.", NormalizedName = "Admin".Normalize() });
+            if (!context.Roles.Any())
+            {
+                context.Roles.Add(new AppIdentityRole
+                {
+                    Name = "DefaultUser",
+                    Description = "Every user is a default user.",
+                    NormalizedName = "DefaultUser".Normalize()
+                });
+                context.Roles.Add(new AppIdentityRole
+                {
+                    Name = "Admin",
+                    Description = "At least one admin is needed.",
+                    NormalizedName = "Admin".Normalize()
+                });
+            }
+
+            var users = context.Users;
+            var roleId = context.Roles.FirstOrDefault()?.Id;
+            foreach (var user in users)
+            {
+                var data = context.UserRoles.Find(user.Id, roleId);
+                if (data == null)
+                    context.UserRoles.Add(new IdentityUserRole<string> { RoleId = roleId, UserId = user.Id });
+            }
             context.SaveChanges();
         }
     }
