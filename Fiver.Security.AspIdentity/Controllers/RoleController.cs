a280b8b07cca76622e763b30e0fd49f3976c32f5
the f <feldrim@gmail.com>
2018-02-11 13:01:31 +0300

    Changed Movies to Roles

---
+++ Fiver.Security.AspIdentity/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Security.AspIdentity.Controllers
+{
+    [Authorize]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<RoleController> _roleManager;
+
+        protected RoleController(RoleManager<RoleController> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var vievModel = _roleManager.Roles.ToList();
+            return View(vievModel);
+        }
+    }
+}
