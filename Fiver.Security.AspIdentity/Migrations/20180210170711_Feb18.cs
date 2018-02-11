5a45a69dab995e4586ec63d233a11311e62ae3cc
the f <feldrim@gmail.com>
2018-02-11 13:02:33 +0300

    Added new migration

---
+++ Fiver.Security.AspIdentity/Migrations/20180210170711_Feb18.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fiver.Security.AspIdentity.Migrations
+{
+    public partial class Feb18 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
