f7fb2d3ff6cc4c44c3d967ae1679f5b1e48815f5
the f <feldrim@gmail.com>
2018-02-16 13:24:16 +0300

    Added new models

---
+++ Security.AspIdentity/Models/BusinessUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Security.AspIdentity.Models
+{
+    public class BusinessUnit
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public BusinessUnit ParentBusinessUnit { get; set; }
+
+        public bool IsRoot { get; set; }
+
+    }
+}
---
+++ Security.AspIdentity/Models/PersonnelUnit.cs
@@ -0,0 +1,15 @@
+﻿namespace Security.AspIdentity.Models
+{
+    public class PersonnelUnit
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public PersonnelUnit ReportsTo { get; set; } 
+
+        public AppIdentityUser UserData { get; set; }
+    }
+}
